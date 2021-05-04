168fe5713012c5bbeed8cf55cff5b3755795b807
Om Chevli <omchevli@gmail.com>
2021-05-04 20:43:51 +0530

    Change Pin

--- src/ChangePin.java
+++ src/ChangePin.java
@@ -2,9 +2,53 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class ChangePin {
     static JPanel changePinPanel = new JPanel();
+
+    JPasswordField txtOldPin = new JPasswordField();
+    JPasswordField txtNewPin = new JPasswordField();
+    JPasswordField txtCPin = new JPasswordField();
+
+    private Connection con = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+
+    public void changePin() {
+        String newPass = String.valueOf(txtNewPin.getPassword());
+        String oldPass = String.valueOf(txtOldPin.getPassword());
+        if (newPass != oldPass) {
+            try {
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "");
+                String infoquery = "SELECT * FROM userdata WHERE AccountNo=? AND Pin=?";
+                ps = con.prepareStatement(infoquery);
+                ps.setString(1, Login.acN);
+                ps.setString(2, oldPass);
+                rs = ps.executeQuery();
+                if (rs.next()) {
+                    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "");
+                    String updateQuery = "UPDATE userdata SET Pin='" + String.valueOf(txtNewPin.getPassword())
+                            + "' WHERE AccountNo='" + Login.acN + "';";
+                    ps = con.prepareStatement(updateQuery);
+                    ps.execute();
+                        JOptionPane.showMessageDialog(changePinPanel, "Pin Updated Successfuly!", "Success!",
+                                JOptionPane.INFORMATION_MESSAGE);
+                }
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(changePinPanel, "Unable to fetch data!", "Error!",
+                        JOptionPane.ERROR_MESSAGE);
+                ex.printStackTrace();
+            }
+        } else {
+            JOptionPane.showMessageDialog(changePinPanel, "Confirm Pin mismatch!", "Failure!",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
 
     ChangePin() {
         changePinPanel.setVisible(false);
@@ -21,11 +65,8 @@ public class ChangePin {
         JLabel confirmPinLabel = new JLabel("Confirm Pin :");
         confirmPinLabel.setFont(new Font("Arial", Font.ITALIC, 20));
 
-        JPasswordField txtOldPin = new JPasswordField();
         txtOldPin.setFont(new Font("Arial", Font.BOLD, 20));
-        JPasswordField txtNewPin = new JPasswordField();
         txtNewPin.setFont(new Font("Arial", Font.BOLD, 20));
-        JPasswordField txtCPin = new JPasswordField();
         txtCPin.setFont(new Font("Arial", Font.BOLD, 20));
 
         JButton btnChangepin = new JButton("Change Pin");
@@ -41,7 +82,13 @@ public class ChangePin {
         txtNewPin.setBounds(500, 250, 200, 30);
         confirmPinLabel.setBounds(300, 300, 150, 30);
         txtCPin.setBounds(500, 300, 200, 30);
-        btnChangepin.setBounds(380,380,180,40);
+        btnChangepin.setBounds(380, 380, 180, 40);
+
+        btnChangepin.addActionListener(new ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                changePin();
+            }
+        });
 
         changePinPanel.setBackground(new Color(239, 251, 198));
 
